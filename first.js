b6ae57885948937dac4d7a84a1e71e3709a9a270
Tanisha <jaiswaltanisha11@gmail.com>
2023-04-20 23:39:38 +0530

    learnt http module

---
+++ first.js
@@ -0,0 +1,9 @@
+console.log("this is a javascript file");
+
+const a = 10;
+var b = 20;
+let c = 30;
+
+console.log(a + b + c);
+
+console.log("I am gojng to run it using nodejs");
---
+++ second.js
@@ -0,0 +1,13 @@
+// using modules in node
+
+const http = require('http');
+
+const server = http.createServer((req, res) =>
+{
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Hey this is a html code</h1>");
+    res.write("<button>Click me</button>")
+    res.end("Ok bye!");
+ })
+
+server.listen(9898);
