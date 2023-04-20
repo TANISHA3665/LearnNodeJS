1009c67e2072141ada6483e95ec1b3a683adba1b
Tanisha <jaiswaltanisha11@gmail.com>
2023-04-21 01:15:09 +0530

    learnt writeFile

--- fileModule.js
+++ fileModule.js
@@ -1,18 +1,38 @@
+const { log } = require("console");
 const fs = require("fs");
 
 
 //Read
-fs.readFile("./abc.txt", (error, data) =>
-{
-    if (error)
-    {
-        console.log(`Error: ${error}`);
-    }
+// fs.readFile("./abc.txt", (error, data) =>
+// {
+//     if (error)
+//     {
+//         console.log(`Error: ${error}`);
+//     }
 
+//     else
+//     {
+//         console.log(data.toString());
+//     }
+// });
+
+// console.log("--------------------------------");
+
+// writeFile
+
+// a) File doesn't exist -> creates a new file and adds the content
+// b) File exists -> deletes the old content and adds the new content
+
+let content = "This is a new dynamic content";
+
+fs.writeFile("newFile.txt", content, (err) =>
+{
+    if (err)
+    {
+        console.log(err);
+    }
     else
     {
-        console.log(data.toString());
+        console.log("saved content");
     }
 })
-
-console.log("--------------------------------")
